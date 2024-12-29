62aca3475b40528449a15aacdf1f9ea03d4018aa
Lucas Silva <lucassilvayoshi@gmail.com>
2024-12-29 10:54:50 -0300

    python

---
+++ combine.py
@@ -0,0 +1,19 @@
+import subprocess
+from sys import argv, exit
+
+
+def extracao():
+    subprocess.run(['python', 'extracao.py'], check=True)
+
+def visualizacao(output_image):
+    subprocess.run(['python', 'visualizacao.py', output_image], check=True)
+
+if __name__ == "__main__":
+    if len(argv) < 2:
+        print("Uso: python combine.py <grafico-combine>")
+        exit(1)
+
+    output_image = f"{argv[1]}.png"
+
+    extracao()
+    visualizacao(output_image)
